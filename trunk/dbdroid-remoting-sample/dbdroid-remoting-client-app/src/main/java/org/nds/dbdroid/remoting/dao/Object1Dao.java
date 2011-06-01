716ceee962b5276cb58ac8135344b16d6c0f9e96
nicolas.dossantos <nicolas.dossantos@8bf2f2e8-d57e-0c4b-278a-68234cb1e1e4>
2011-06-01 10:17:34 +0000

    move sample client app to dbdroid-remoting-sample

---
+++ trunk/dbdroid-remoting-sample/dbdroid-remoting-client-app/src/main/java/org/nds/dbdroid/remoting/dao/Object1Dao.java
@@ -0,0 +1,13 @@
+package org.nds.dbdroid.remoting.dao;
+
+import org.nds.dbdroid.DataBaseManager;
+import org.nds.dbdroid.dao.AndroidDAO;
+import org.nds.dbdroid.remoting.entity.Object1;
+
+public class Object1Dao extends AndroidDAO<Object1, Integer> {
+
+    public Object1Dao(DataBaseManager dbManager) {
+        super(dbManager);
+    }
+
+}
